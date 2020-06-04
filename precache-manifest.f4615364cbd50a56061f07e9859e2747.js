471fafb175f4412170f783f850539ddbd46b0677
jester_adduru <jesteradduru@gmail.com>
2020-06-04 11:19:05 +0800

    Updates

---
+++ precache-manifest.f4615364cbd50a56061f07e9859e2747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd68be25a4b3cc53bc0ea1f36667d78",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "adeb963866d2be5b2550",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e352ceb40d752131e664",
+    "url": "/robofriends/static/css/main.354043f8.chunk.css"
+  },
+  {
+    "revision": "adeb963866d2be5b2550",
+    "url": "/robofriends/static/js/2.e667d092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e667d092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e352ceb40d752131e664",
+    "url": "/robofriends/static/js/main.c1b6ffac.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
